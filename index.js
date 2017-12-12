bca7c6acb471b6d64b1828724a7b716dd2a6a3ee
Broda Noel <brodanoel@gmail.com>
2017-12-12 18:46:10 -0300

    Reimplement endpoints

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
-var express = require('express');
-var app = express();
-var utils = require('./libs/utils.js');
-const PORT = 9001;
-const baseUrl = 'https://api.mercadolibre.com';
+const express = require('express');
+const app = express();
+const config = require('./config.js');
+const items = require('./api/items');
 
 /**
- * This endpoint retrieve the list of element that were retrieved by the
+ * This endpoint retrieve the list of elements that were retrieved by the
  * https://api.mercadolibre.com/sites/MLA/search?q=xxx endpoint.
  * Documentation available: https://api.mercadolibre.com/sites/MLA/search?q=test#options
  */
-app.get('/api/items', function(req, res) {
-  fetch(
-    `${baseUrl}/sites/MLA/search?q=${req.query.q}`,
-    { method: 'GET' }
-  ).then(response => response.json())
-  .then(response => {
-    const items = response.results.map(item => ({
-      id: item.id,
-      title: item.title,
-      price: {
-        currency: item.currency_id,
-        amount: parseInt(item.price),
-        decimals: utils.getDecimals(item.price)
-      },
-      picture: item.thumbnail,
-      condition: item.condition,
-      free_shipping: item.shipping.free_shipping
-    }));
+app.get('/api/items', items.query);
 
-    res.send({
-      status: 'ok',
-      data: {
-        items
-      }
-    });
-  });
-});
+/**
+ * This endpoint retrieve the element detail for a given element id.
+ * http://localhost:9001/api/items/MLA673538490
+ * Documentation available: https://api.mercadolibre.com/items/MLA673538490#json
+ */
+app.get('/api/items/:id', items.id);
 
-app.listen(PORT);
-console.log(`Express running on port ${PORT}`);
+app.listen(config.port);
+console.log(`Express running on port ${config.port}`);
