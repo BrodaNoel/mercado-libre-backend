bc8416a4e52e2f4b76868b9270d3c59d724da5b1
Broda Noel <brodanoel@gmail.com>
2017-12-12 18:07:50 -0300

    Add /api/items endpoint

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 var express = require('express');
 var app = express();
+var utils = require('./libs/utils.js');
 const PORT = 9001;
+const baseUrl = 'https://api.mercadolibre.com';
 
-app.get('/api/items', function(req, res){
-  res.send({ foo: 'bar' });
+/**
+ * This endpoint retrieve the list of element that were retrieved by the
+ * https://api.mercadolibre.com/sites/MLA/search?q=xxx endpoint.
+ * Documentation available: https://api.mercadolibre.com/sites/MLA/search?q=test#options
+ */
+app.get('/api/items', function(req, res) {
+  fetch(
+    `${baseUrl}/sites/MLA/search?q=${req.query.q}`,
+    { method: 'GET' }
+  ).then(response => response.json())
+  .then(response => {
+    const items = response.results.map(item => ({
+      id: item.id,
+      title: item.title,
+      price: {
+        currency: item.currency_id,
+        amount: parseInt(item.price),
+        decimals: utils.getDecimals(item.price)
+      },
+      picture: item.thumbnail,
+      condition: item.condition,
+      free_shipping: item.shipping.free_shipping
+    }));
+
+    res.send({
+      status: 'ok',
+      data: {
+        items
+      }
+    });
+  });
 });
 
 app.listen(PORT);
